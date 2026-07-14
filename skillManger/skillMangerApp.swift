e1696abfd22183f97e1165502935544359e47073
ming <2290320152@qq.com>
2026-07-14 16:30:23 +0800

    Initial Commit

---
+++ skillManger/skillMangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  skillMangerApp.swift
+//  skillManger
+//
+//  Created by ming on 2026/7/14.
+//
+
+import SwiftUI
+
+@main
+struct skillMangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ skillMangerTests/skillMangerTests.swift
@@ -0,0 +1,17 @@
+//
+//  skillMangerTests.swift
+//  skillMangerTests
+//
+//  Created by ming on 2026/7/14.
+//
+
+import Testing
+@testable import skillManger
+
+struct skillMangerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
